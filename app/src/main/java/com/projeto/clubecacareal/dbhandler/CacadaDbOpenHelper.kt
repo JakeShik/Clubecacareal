c9b2eaddae9fea666347c2e343427a1945b7e0df
JakeShik <jaha5825@gmail.com>
2022-06-29 21:03:30 +0100

    Created a package for all the db files. Also created the CacadaDbOpenHelper.kt

---
+++ app/src/main/java/com/projeto/clubecacareal/dbhandler/CacadaDbOpenHelper.kt
@@ -0,0 +1,25 @@
+package com.projeto.clubecacareal.dbhandler
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class CacadaDbOpenHelper(context: Context?) : SQLiteOpenHelper(context, NAME,null,VERSION) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        requireNotNull(db)
+
+        //CacadasDbTable(db).create()
+        //CacadoresDbTable(db).create()
+        //RegiaoCacaDbTable(db).create()
+        //InscricaoCacaDbTable(db).create()
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+    companion object{
+        const val NAME = "books.db"
+        const val VERSION = 1
+    }
+}
