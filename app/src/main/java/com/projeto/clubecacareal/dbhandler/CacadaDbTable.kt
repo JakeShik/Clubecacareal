dff45c54b30ea2a8634a5fb5dcc8d9ffb1d2f5c8
JakeShik <jaha5825@gmail.com>
2022-06-30 21:24:38 +0100

    Created the name and fields, not yet fully implemented. Need to create the class for regiao table.

--- app/src/main/java/com/projeto/clubecacareal/dbhandler/CacadaDbTable.kt
+++ app/src/main/java/com/projeto/clubecacareal/dbhandler/CacadaDbTable.kt
@@ -1,6 +1,25 @@
 package com.projeto.clubecacareal.dbhandler
 
 import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
 
-class CacadaDbTable(db: SQLiteDatabase) : DbTable {
+class CacadaDbTable(db: SQLiteDatabase) : DbTable(db, NAME) {
+    override fun create() {
+        db.execSQL("CREATE TABLE $name(${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREFFIELD_" +
+                "$FIELD_NUM_CACADORES INTEGER NOT NULL, " +
+                "$FIELD_DATA DATE " +
+                "$FIELD_LOCALIZACAO INTEGER NOT NULL," +
+                "")
+    }
+    companion object {
+        const val NAME = "cacada"
+
+
+        const val FIELD_NUM_CACADORES = "num_cacadores"
+        const val FIELD_DATA = "data"
+        const val FIELD_LOCALIZACAO = "localizacao"
+        const val FIELD_REGIAO_ID = "regiao_id"
+
+        val ALL_FIELDS = arrayOf(BaseColumns._ID, FIELD_NUM_CACADORES, FIELD_DATA, FIELD_LOCALIZACAO, FIELD_REGIAO_ID)
+    }
 }
