4f1361211ec9efddd4d7f870f83b93c22f2badb5
JakeShik <jaha5825@gmail.com>
2022-07-02 13:42:11 +0100

    Table for Regions created and fully implemented for now.

---
+++ app/src/main/java/com/projeto/clubecacareal/dbhandler/RegiaoCacaDbTable.kt
@@ -0,0 +1,22 @@
+package com.projeto.clubecacareal.dbhandler
+
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class RegiaoCacaDbTable(db: SQLiteDatabase) : DbTable(db, NAME) {
+    override fun create() {
+        db.execSQL("CREATE TABLE $name(${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "$FIELD_LOCALIZACAO INTEGER NOT NULL, " +
+                "$FIELD_NOMEANIMAL TEXT NOT NULL, " +
+                "$FIELD_QUANTIDADEANIMAIS INTEGER NOT NULL)")
+    }
+    companion object {
+        const val NAME = "regiaoCaca"
+        const val FIELD_LOCALIZACAO = "localizacao"
+        const val FIELD_NOMEANIMAL = "nomeAnimal"
+        const val FIELD_QUANTIDADEANIMAIS = "quantidadeAnimais"
+
+
+        val ALL_FIELDS = arrayOf(BaseColumns._ID, FIELD_LOCALIZACAO, FIELD_NOMEANIMAL, FIELD_QUANTIDADEANIMAIS)
+    }
+}
