e0b7dc038b77d55a6a2e32f8539a569e05293e9d
JakeShik <jaha5825@gmail.com>
2022-07-06 11:35:06 +0100

    RegiaoCaca implemented cursor.

--- app/src/main/java/com/projeto/clubecacareal/RegiaoCaca.kt
+++ app/src/main/java/com/projeto/clubecacareal/RegiaoCaca.kt
@@ -1,6 +1,8 @@
 package com.projeto.clubecacareal
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 import com.projeto.clubecacareal.dbhandler.RegiaoCacaDbTable
 
 data class RegiaoCaca(
@@ -17,5 +19,24 @@ data class RegiaoCaca(
     values.put(RegiaoCacaDbTable.FIELD_QUANTIDADEANIMAIS, quantidadeAnimais)
 
     return values
+  }
+
+
+  companion object{
+    fun fromCursor(cursor: Cursor) : RegiaoCaca {
+      val posId=cursor.getColumnIndex(BaseColumns._ID)
+      val posLocalizacao = cursor.getColumnIndex(RegiaoCacaDbTable.FIELD_LOCALIZACAO)
+      val posNomeAnimal = cursor.getColumnIndex(RegiaoCacaDbTable.FIELD_NOMEANIMAL)
+      val posQuantidadeAnimais = cursor.getColumnIndex(RegiaoCacaDbTable.FIELD_QUANTIDADEANIMAIS)
+
+
+      val id = cursor.getLong(posId)
+      val localizacao = cursor.getLong(posLocalizacao)
+      val nomeAnimal = cursor.getString(posNomeAnimal)
+      val quantidadeAnimais = cursor.getLong(posQuantidadeAnimais)
+
+
+      return RegiaoCaca(localizacao, nomeAnimal, quantidadeAnimais, id)
+    }
   }
 }
